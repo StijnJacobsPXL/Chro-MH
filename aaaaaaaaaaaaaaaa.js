fcdd9323d63908bd15b81728b428f3ebdcf72106
StijnJacobsPXL <56969978+StijnJacobsPXL@users.noreply.github.com>
2022-10-15 22:46:33 +0000

    AAAAA

--- aaaaaaaaaaaaaaaa.js
+++ aaaaaaaaaaaaaaaa.js
@@ -72,3 +72,59 @@ for (let i in locations){
     
     await new Promise(r => setTimeout(r, 1000));
 }
+
+
+
+
+// ty tsitu :)
+// Step 1: get data from server
+async function getCatchStats(){
+    let cookiematerial = {};
+    let data;
+    const xhr = new XMLHttpRequest();
+    xhr.open(
+        "GET",
+        `https://www.mousehuntgame.com/managers/ajax/mice/getstat.php?sn=Hitgrab&hg_is_ajax=1&action=get_hunting_stats&uh=${user.unique_hash}`);
+    xhr.onload = function() {
+        data = JSON.parse(xhr.responseText);
+        for(let j in data.hunting_stats){
+            console.log(data.hunting_stats[j]);
+            cookiematerial[data.hunting_stats[j].name] = {'catches': data.hunting_stats[j].num_catches, 'thumb':data.hunting_stats[j].thumb }
+        };
+    }
+    xhr.send();
+    await new Promise(r => setTimeout(r, 1000));
+    localStorage.setItem("CMT_catchstats", JSON.stringify(cookiematerial));
+}
+
+
+// Take cookie & filter
+async function listMiceOfCurrentLocation(){
+    let currentLocation = user.environment_type;
+    let mice= [];
+    const xhr = new XMLHttpRequest();
+    xhr.open(
+        "POST",
+        `https://www.mousehuntgame.com/managers/ajax/mice/mouse_list.php?action=get_environment&category=${currentLocation}&user_id=${user.user_id}&display_mode=stats&view=ViewMouseListEnvironments&uh=${user.unique_hash}`,
+        true
+        );
+    xhr.onload = function (){
+        const response = JSON.parse(xhr.responseText);
+        const stats = response.mouse_list_category.subgroups[0].mice;
+        console.log(stats);
+        if (!stats) {
+            console.log(`no stats found for ${currentLocation}`);
+        }else{
+            stats.forEach(el => {mice.push(el.name);});
+
+        }
+    }
+    xhr.send();
+    await new Promise(r => setTimeout(r, 1000));
+    let storedData = JSON.parse(localStorage.getItem("CMT_catchstats"));
+    mice.forEach(el=>{
+        console.log(storedData[el])
+    }
+)
+console.log(storedData)
+}
